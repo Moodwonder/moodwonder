e050fce023f8a84c3f00528effe1ef394bb62b89
Praveen <praveen.ts@titechnologies.org>
2015-07-15 15:49:56 +0530

    initial push

---
+++ server/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var fs = require('fs');
+var mongoose = require('mongoose');
+var app = express();
+var server = require('http').Server(app);
+var passport = require('passport');
+var secrets = require('./config/secrets');
+
+// Find the appropriate database to connect to, default to localhost if not found.
+var connect = function() {
+	mongoose.connect(secrets.db, function(err, res) {
+		if(err) {
+			console.log('Error connecting to: ' + secrets.db + '. ' + err);
+		}else {
+			console.log('Succeeded connected to: ' + secrets.db);
+		}
+	});
+};
+connect();
+
+//delete mongoose.connection.models['User'];
+
+mongoose.connection.on('error', console.log);
+mongoose.connection.on('disconnected', connect);
+
+
+// Bootstrap models
+fs.readdirSync(__dirname + '/models').forEach(function(file) {
+	if(~file.indexOf('.js')) require(__dirname + '/models/' + file);
+});
+
+
+server.listen(app.get('port'));
