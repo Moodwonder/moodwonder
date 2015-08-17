5da334452bfd75129ab777136aee0c93f25507fb
Praveen <praveen.ts@titechnologies.org>
2015-08-17 18:43:24 +0530

    es6 fixes

--- app/components/App.react.js
+++ app/components/App.react.js
@@ -4,7 +4,7 @@ import Navigation from 'components/Navigation.react';
 
 export default class App extends React.Component {
   render() {
-    return (
+      return (
       <div>
         <Navigation />
         <RouteHandler />
