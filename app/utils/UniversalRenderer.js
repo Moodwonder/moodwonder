eb48a279255cd9e4b725c292ef64cfbece6e31a4
Praveen <praveen.ts@titechnologies.org>
2015-08-17 17:44:59 +0530

    es6 fixes

--- app/utils/UniversalRenderer.js
+++ app/utils/UniversalRenderer.js
@@ -3,39 +3,38 @@ import React from 'react';
 import Router from 'react-router';
 
 const renderToMarkup = (alt, routes, state, url) => {
-  let markup;
+    let markup;
 
-  Router.run(routes, url, (Handler) => {
-    alt.bootstrap(state);
-    let content = React.renderToString(React.createElement(Handler));
-    markup = Iso.render(content, alt.flush());
-  });
-  return markup;
+    Router.run(routes, url, (Handler) => {
+        alt.bootstrap(state);
+        let content = React.renderToString(React.createElement(Handler));
+        markup = Iso.render(content, alt.flush());
+    });
+    return markup;
 };
 
 export default function UniversalRenderer(alt, routes, html) {
-  let render;
-  if (typeof window === 'undefined') {
+    let render;
+    if (typeof window === 'undefined') {
 
-    if (html) {
-      render = (state, url) => {
-        const markup = renderToMarkup(alt, routes, state, url);
-        return html.replace('CONTENT', markup);
-      };
+        if (html) {
+              render = (state, url) => {
+                  const markup = renderToMarkup(alt, routes, state, url);
+                  return html.replace('CONTENT', markup);
+              };
+        } else {
+              render = (state, url) => {
+                  return renderToMarkup(alt, routes, state, url);
+              };
+        }
     } else {
-      render = (state, url) => {
-        return renderToMarkup(alt, routes, state, url);
-      };
+        render = Iso.bootstrap((state, _, container) => {
+            alt.bootstrap(state);
+            Router.run(routes, Router.HistoryLocation, (Handler) => {
+                let node = React.createElement(Handler);
+                React.render(node, container);
+            });
+        });
     }
-  } else {
-    render = Iso.bootstrap((state, _, container) => {
-      alt.bootstrap(state);
-      Router.run(routes, Router.HistoryLocation, (Handler) => {
-        let node = React.createElement(Handler);
-        React.render(node, container);
-      });
-    });
-  }
-
-  return render;
+    return render;
 }
