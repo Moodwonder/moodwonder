fe0b903e57d25dd46337025984035cbd63adf2b6
Praveen <praveen.ts@titechnologies.org>
2015-08-19 12:56:47 +0530

    new layout for admin

---
+++ app/components/login/adminlogin.react.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import Immutable from 'immutable';
+import UserActions from 'actions/UserActions';
+import UserStore from 'stores/UserStore';
+import { MyOwnInput } from 'components/Formsy-components';
+import { Navigation } from 'react-router';
+import mixins from 'es6-mixins';
+
+export default class Adminlogin extends React.Component {
+
+  constructor(props) {
+      super(props);
+      mixins(Navigation, this);
+      this.state = UserStore.getState();
+      this.state.canSubmit = false;
+      this.validationErrors = {};
+  }
+
+  componentDidMount() {
+      UserStore.listen(this._onChange);
+  }
+
+  componentWillUnmount() {
+      UserStore.unlisten(this._onChange);
+  }
+
+  enableButton = () => {
+      this.setState({canSubmit: true});
+  }
+
+  disableButton = () => {
+      this.setState({canSubmit: false});
+  }
+
+  _onChange = (state) => {
+      this.setState(state);
+      if(this.state.isLoggedIn){
+          window.location.assign('/survey');
+          //this.context.router.transitionTo('/survey');
+      }
+  }
+
+  _onLoginSubmit = (model) => {
+      let email = model.email.trim();
+      let password = model.password.trim();
+      UserActions.manuallogin({
+        email: email,
+        password: password
+      });
+  }
+
+  render() {
+      let renderedResult;
+      let message;
+
+      if (this.state.message !== '') {
+          message = (
+                <div className="login__container">
+                      <fieldset className="login__fieldset">
+                         <div className="alert alert-info">
+                              {this.state.message}
+                         </div>
+                      </fieldset>
+                </div>
+            );
+      }
+
+      if (this.state.isLoggedIn) {
+          let currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+          renderedResult = (
+            <div className="container">
+              <h2 className="login__header">Welcome {currentUser.name}</h2>
+            </div>
+          );
+
+      }else {
+          if (this.state.isLogginWaiting) {
+              message = (<h3 className="login__header">Processing...</h3>);
+          }
+
+          renderedResult = (
+          <div className="container">
+            <h2>Admin login..</h2>
+            {message}
+                <Formsy.Form onValidSubmit={this._onLoginSubmit} onValid={this.enableButton} onInvalid={this.disableButton} >
+                   <MyOwnInput
+                   name="email"
+                   className="form-control"
+                   placeholder="Email"
+                   validations="isEmail"
+                   validationError="This is not a valid email"
+                   required/>
+                   <MyOwnInput
+                   type="password"
+                   name="password"
+                   className="form-control"
+                   placeholder="Password"
+                   validationError="This is not a valid email"
+                   required/>
+                   <button type="submit" className="btn btn-default" disabled={!this.state.canSubmit}>Submit</button>
+                </Formsy.Form>
+          </div>
+        );
+
+      }
+
+      return (
+          <div className="login">
+            {renderedResult}
+          </div>
+      );
+
+  }
+}
+
+Adminlogin.propTypes = { user: React.PropTypes.instanceOf(Immutable.Map) };
+Adminlogin.contextTypes = { router: React.PropTypes.func };
--- app/routes.js
+++ app/routes.js
@@ -20,31 +20,41 @@ import Takesurvey from 'components/customsurvey/Takesurvey.react';
 import NotFound from 'components/404.react';
 import Languages from 'components/language/Languages.react';
 import Test1 from 'components/test1';
+import Admin from 'components/Admin.react';
+import Adminlogin from 'components/login/adminlogin.react';
+// import Dashboard from 'components/dashboard/dashboard.react';
 
 const routes = (
-  <Route name="app" path="/" handler={App} >
-    <Route name="login" handler={Login} />
-    <Route name="logout" handler={Logout} />
-    <Route name="index" handler={Index} />
-    <Route name="signup" handler={Signup} />
-    <Route name="signup/:hash" handler={Signup} />
-    <Route name="forgotpassword" handler={ForgotPassword} />
-    <Route name="createpassword/:hash" handler={CreatePassword} />
-    <Route name="customsurvey" handler={Customsurvey} />
-    <Route name="surveyforms" handler={Surveyforms} />
-    <Route name="takesurvey/:key" handler={Takesurvey} />
-    <Route name="survey" handler={Survey} />
-    <Route name="myprofile" handler={MyProfile} />
-    <Route name="myteam" handler={MyTeam} />
-    <Route name="tabs" handler={Tabs} />
-    <Route name="mycompany" handler={MyCompanyInfo} />
-    <Route name="mymanager" handler={MyManagerInfo} />
-    <Route name="languages" handler={Languages} />
-    <Route name="test1" handler={Test1} />
-    <DefaultRoute handler={Index} />
-    <NotFoundRoute name="404" handler={NotFound} />
+  <Route>
+    <Route name="appadmin" path="/admin" handler={Admin} >
+        <Route name="admin" handler={Adminlogin} />
+        <Route name="/admin/login" handler={Adminlogin} />
+        <DefaultRoute handler={Adminlogin} />
+        <NotFoundRoute name="404page" handler={NotFound} />
+    </Route>
+    <Route name="app" path="/" handler={App} >
+      <Route name="login" handler={Login} />
+      <Route name="logout" handler={Logout} />
+      <Route name="index" handler={Index} />
+      <Route name="signup" handler={Signup} />
+      <Route name="signup/:hash" handler={Signup} />
+      <Route name="forgotpassword" handler={ForgotPassword} />
+      <Route name="createpassword/:hash" handler={CreatePassword} />
+      <Route name="customsurvey" handler={Customsurvey} />
+      <Route name="surveyforms" handler={Surveyforms} />
+      <Route name="takesurvey/:key" handler={Takesurvey} />
+      <Route name="survey" handler={Survey} />
+      <Route name="myprofile" handler={MyProfile} />
+      <Route name="myteam" handler={MyTeam} />
+      <Route name="tabs" handler={Tabs} />
+      <Route name="mycompany" handler={MyCompanyInfo} />
+      <Route name="mymanager" handler={MyManagerInfo} />
+      <Route name="languages" handler={Languages} />
+      <Route name="test1" handler={Test1} />
+      <DefaultRoute handler={Index} />
+      <NotFoundRoute name="404" handler={NotFound} />
+    </Route>
   </Route>
 );
-
 
 export default routes;
