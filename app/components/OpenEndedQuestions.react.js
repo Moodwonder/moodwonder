d4f40b4f1d61b28872dd29db7d66374d45417530
Praveen <praveen.ts@titechnologies.org>
2015-09-29 16:39:11 +0530

    es6 fixes

--- app/components/OpenEndedQuestions.react.js
+++ app/components/OpenEndedQuestions.react.js
@@ -58,7 +58,7 @@ export default class OpenEndedQuestions extends React.Component {
 
   render() {
       let savesurveyflag = this.state.savesurveyflag;
-      
+
       let statusmessage = '';
       if(savesurveyflag) {
           statusmessage = (<div className="alert alert-success">
