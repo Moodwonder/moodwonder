a8f7beb4e47fd0742a4c0fdd38c6f289c4613a60
Sijo Vijayan <sijo.vijayan@titechnologies.in>
2016-01-07 11:10:22 +0530

    Request demo test

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
     //staticUrl: 'http://localhost:3000/'
     staticUrl: 'http://moodwonder.titechnologies.org:9039/',
     fromEmail: 'admin@moodwonder.com',
-    adminemail: 'ujjwal.mairh@gmail.com'
+    adminemail: 'ujjwal.mairh@gmail.com,bindiya.prakash@titechnologies.in'
 };
