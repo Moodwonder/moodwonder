3a4dfafa4953cd8b981b81ea0787a94690eead64
mairh <ujjwal.mairh@gmail.com>
2016-01-19 18:58:51 +0200

    adding kimmo email to admin email list

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
     //staticUrl: 'http://localhost:3000/'
     staticUrl: 'http://moodwonder.titechnologies.org:9039/',
     fromEmail: 'admin@moodwonder.com',
-    adminemail: 'ujjwal.mairh@gmail.com,bindiya.prakash@titechnologies.in'
+    adminemail: 'ujjwal.mairh@gmail.com,info@moodwonder.com,kimmo_kauhanen@hotmail.com,bindiya.prakash@titechnologies.in'
 };
