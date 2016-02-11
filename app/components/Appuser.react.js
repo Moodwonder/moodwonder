a8f4425455e40064f5ee45249a5ed4c9c446870a
Praveen <praveen.ts@titechnologies.org>
2016-02-11 13:10:37 +0530

    checking

--- app/components/Appuser.react.js
+++ app/components/Appuser.react.js
@@ -5,7 +5,7 @@ import Navigation from 'components/Navigation.react';
 import Leftnav from 'components/Leftnav.react';
 import Rightnav from 'components/Rightnav.react';
 import SidebarMenu from 'components/SidebarMenu.react';
-import GoogleAnalytics from "components/analytics/GoogleAnalytics.react";
+//import GoogleAnalytics from "components/analytics/GoogleAnalytics.react";
 
 export default class App extends React.Component {
 
@@ -117,8 +117,8 @@ export default class App extends React.Component {
                             </div>
                             {rightnav}
                         </div>
-                    </div>,
-                    <GoogleAnalytics id="UA-40351687-1" />
+                    </div>
+                    //<GoogleAnalytics id="UA-40351687-1" />
               ];
 
       return (
