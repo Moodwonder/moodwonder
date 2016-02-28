f2435c189125ac0eabbb2624c1559fb5050124bb
mairh <ujjwal.mairh@gmail.com>
2016-02-28 21:23:03 +0200

    lint fix component/pages

--- app/constants/LanguageConstants.js
+++ app/constants/LanguageConstants.js
@@ -1,19 +1,7 @@
-/**
- * TaskConstants
- */
-
-//const keyMirror = require('keymirror');
-//
-//module.exports = keyMirror({
-//  EN: null,
-//  FI: null,
-//  english: null
-//});
-
 import keyMirror from 'keymirror';
 
 export default keyMirror({
-  EN: null,
-  FI: null,
-  english: null
+    EN: null,
+    FI: null,
+    english: null
 });
